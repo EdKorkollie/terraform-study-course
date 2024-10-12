9edcaa513d1e5b2d74c048798aef5495ae7d9e25
edkorkollie <korkollieed@gmail.com>
2024-10-11 22:30:56 -0400

    initial commit

---
+++ Terraform-for-AWS-cloud/EBS-Volume/variable.tf
@@ -0,0 +1,26 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+    type = map(string)
+    default = {
+      "us-east-1" = "ami-053053586808c3e70"
+      "us-east-2" = "ami-068cf3d51efeb20d6"
+      "us-west-2" = "ami-0a5d7c68997d6ab3a"
+      "us-west-1" = "ami-082ebbea44fc7abcd"
+    }
+  
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "UBUNTU"
+}
