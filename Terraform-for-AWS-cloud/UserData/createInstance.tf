64004135fce7fb4ecfb3ee6895698aa433c388fa
edkorkollie <korkollieed@gmail.com>
2024-10-11 23:04:27 -0400

    userdata

---
+++ Terraform-for-AWS-cloud/UserData/createInstance.tf
@@ -0,0 +1,23 @@
+resource "aws_key_pair" "levelup_key" {
+  key_name   = "levelup_key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami               = lookup(var.AMIS, var.AWS_REGION)
+  availability_zone = "us-east-1a"
+  key_name          = aws_key_pair.levelup_key.key_name
+
+  instance_type = "t2.micro"
+
+  user_data = file("installapache.sh")
+
+  tags = {
+    Name = "custome_instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.myFirstInstance
+}
+
