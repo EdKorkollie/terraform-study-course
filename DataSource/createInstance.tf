0c5203ad5bd81fa569395cbad85c413be2d2b12c
edkorkollie1990 <korkollieed@gmail.com>
2024-10-16 02:34:31 -0400

    remote state

--- DataSource/createInstance.tf
+++ DataSource/createInstance.tf
@@ -1,7 +1,7 @@
-data "aws_availability_zones" "available" {} # this will search all the AWS Availability zones and store it in this  variable
+data "aws_availability_zones" "available" {} # this will search all the AWS Availability zones and store it in this  variable (available)
 
 data "aws_ami" "latest_ubuntu" {
-  most_recent = true
+  most_recent = true # it means it will fetch for the most recent ubuntu AMI
   owners = [ "amazon" ]
 
   filter {
@@ -28,7 +28,7 @@ data "aws_ami" "latest_ubuntu" {
 resource "aws_instance" "myFirstInstance" {
     ami               = data.aws_ami.latest_ubuntu.id
     instance_type = "t2.micro"
-    availability_zone = data.aws_availability_zones.available.names[1]
+    availability_zone = data.aws_availability_zones.available.names[1] #means chose the availablity zone which is present at index 1
 
     tags= {
         name= "custom_instance"
